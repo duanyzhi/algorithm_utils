e0db247c12683da8d0c26dbbb49026db8771b45c
duanyzhi <duanyzhi@outlook.com>
2023-06-02 10:56:15 +0800

    support base tensor

--- core/alu_test.cc
+++ core/alu_test.cc
@@ -10,12 +10,13 @@ void test_alu() {
   int kernel_size = 3;
   auto kernel_tensor = alu::cv::Get2DGaussianKernel(kernel_size);
 
-  double* data_ptr = static_cast<double*>(kernel_tensor.GetTensorBuffer()->data()); 
+  double* data_ptr = static_cast<double*>(kernel_tensor.data()); 
 
   for (int i = 0; i < 9; i++) {
     std::cout << *data_ptr << " ";
     data_ptr++;
   }  
+  std::cout << "\n";
 }
 
 }
--- cv/core/methods.cc
+++ cv/core/methods.cc
@@ -7,13 +7,12 @@ namespace cv {
 
 Tensor Get2DGaussianKernel(const int& kernel_size, double deviation) {
   Tensor kernel_tensor(kernel_size, kernel_size, alu::AluType::ADOUBLE);
-  double* data_ptr = static_cast<double*>(kernel_tensor.GetTensorBuffer()->data());
+  double* data_ptr = static_cast<double*>(kernel_tensor.data());
   int range = kernel_size / 2;
-  LOG("range " << range);
   for (int x = -range; x < range + 1; x++) {
     for (int y = -range; y < range + 1; y++) {
       auto value = alu::math::GaussianDistribution2D(x, y, deviation);
-      LOG(value);
+      // LOG(value);
       *data_ptr = value;
       data_ptr++;
     }
